d10aa10a017240d2f291a1838610b3b763c962fb
protoeng <104053985+protoeng@users.noreply.github.com>
2020-10-08 09:36:17 +0000

    Improved markdown

--- Assignment.py
+++ Assignment.py
@@ -63,3 +63,5 @@ for word in doc_copy.split(" "):
 #Removing new-line characters and punctuations
 p = re.compile(r'(\n)|(\r)|(\t)|([!"#$%&()*+,-./:;<=>?@\[\\\]^_`{|}~])', re.IGNORECASE)
 doc=re.sub(p,' ',doc)
+
+# <codecell>
